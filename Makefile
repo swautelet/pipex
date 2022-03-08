ab1e324db807e9ee80e571c2b510587dddd10115
SWautele <swautele@student.s19.be>
2022-03-08 21:08:12 +0100

    autosave

---
+++ Makefile
@@ -0,0 +1,29 @@
+SRC = src/pipex.c \
+	src/ft_split.c \
+	src/calloc.c
+CC = gcc
+CFLAGS = -Werror -Wall -Wextra
+HEADER = pipex.h
+NAME = pipex
+OBJET = $(SRC:.c = .o)
+
+all : $(NAME)
+
+$(NAME) : $(OBJET)
+	$(CC) $(CFLAGS) $(OBJET) -o $(NAME)
+
+.o : .c
+	$(CC) $(CFLAGS) $? -c -o $(OBJET)
+
+clean :
+	rm -f $(OBJET)
+
+fclean : clean
+	rm -f $(NAME)
+
+save : fclean
+	git add *
+	git commit -m autosave
+	git push https://github.com/swautelet/pipex.git
+
+.phony : all $(NAME) clean fclean
---
+++ src/pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/04 19:19:49 by swautele          #+#    #+#             */
+/*   Updated: 2022/03/08 20:48:47 by swautele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <stdlib.h>
+
+char	**ft_split(char const *str, char c);
+int		find_path(char **envp);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+int		ft_strlen(char *str);
+int		ft_strlen(char *str);
+
+#endif
