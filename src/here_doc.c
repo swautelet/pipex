094ac2c80731949cd79bb97a17c0b0a45b8ae556
SWautele <swautele@student.s19.be>
2022-03-14 19:59:11 +0100

    autosave

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 18:12:31 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/14 19:45:47 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/14 19:52:03 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,6 @@ void	wr_heredoc(int fd, char *end)
 int	ft_here_doc(int argc, char **argv, char **envp)
 {
 	t_read	r;
-	int		id;
 
 	if (argc < 5)
 		return (0);
@@ -69,6 +68,7 @@ int	ft_here_doc(int argc, char **argv, char **envp)
 	wr_heredoc(r.fd[r.i], argv[r.i]);
 	close (r.fd[r.i]);
 	r.fd[r.i] = open("iwehiuwfiewchu.tmp", O_RDONLY, 00777);
+	unlink("iwehiuwfiewchu.tmp");
 	r.out = open(argv[argc - 1], O_CREAT | O_WRONLY | O_APPEND, 00644);
 	if (r.fd[r.i] == -1 || r.out == -1)
 		return (-1);
@@ -78,9 +78,6 @@ int	ft_here_doc(int argc, char **argv, char **envp)
 		dup2(r.fd[r.i - 1], 0);
 		r.fd[r.i] = prep_command(argv[r.i], envp);
 	}
-	id = fork();
-	if (id == 0)
-		execve("/bin/rm", ft_split("rm iwehiuwfiewchu.tmp", ' '), envp);
 	write_and_exit(r, 2);
 	return (0);
 }
